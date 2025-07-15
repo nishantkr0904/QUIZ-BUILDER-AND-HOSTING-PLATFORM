b46eb7b645e67b3ea8d7e3df10425fa1c548eb08
Nishant Kumar <nishantkr.0904@gmail.com>
2025-07-15 17:37:06 +0530

    Added welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz Builder and Hosting Platform</title>
+</head>
+<body>
+    <h1>Welcome to Quiz Builder and Hosting Platform!</h1>
+    <p>If you see this page, your Laravel backend is working.</p>
+    <p>To show quizzes, connect your QuizController and home.blade.php.</p>
+</body>
+</html>
