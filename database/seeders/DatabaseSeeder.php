e784324c7e2a8b8883c5e6a7876a2cd2b3cf2380
Nishant Kumar <nishantkr.0904@gmail.com>
2025-07-15 17:34:44 +0530

    Updated DatabseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,6 +10,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->admin()->create([
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+        ]);
+        \App\Models\User::factory(5)->create();
+        \App\Models\Category::factory(3)->create()->each(function ($category) {
+            \App\Models\Quiz::factory(2)->create(['category_id' => $category->id])->each(function ($quiz) {
+                \App\Models\Question::factory(5)->create(['quiz_id' => $quiz->id]);
+            });
+        });
     }
 }
